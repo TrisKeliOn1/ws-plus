4dfeaf5708d05a45eb1da8a0e63249079c9667ef
hebert <hebertLandim007@outlook.com>
2025-02-13 11:47:24 -0300

    feat: add BadRequestException

---
+++ src/main/java/com/client/ws/wsplus/exception/BadRequestException.java
@@ -0,0 +1,8 @@
+package com.client.ws.wsplus.exception;
+
+public class BadRequestException extends RuntimeException{
+
+    public BadRequestException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/client/ws/wsplus/service/impl/SubscriptionTypeServiceImpl.java
+++ src/main/java/com/client/ws/wsplus/service/impl/SubscriptionTypeServiceImpl.java
@@ -2,12 +2,14 @@ package com.client.ws.wsplus.service.impl;
 
 import com.client.ws.wsplus.dto.SubscriptionTypeDto;
 import com.client.ws.wsplus.exception.NotFoundException;
+import com.client.ws.wsplus.exception.BadRequestException;
 import com.client.ws.wsplus.model.SubscriptionType;
 import com.client.ws.wsplus.repository.SubscriptionTypeRepository;
 import com.client.ws.wsplus.service.SubscriptionTypeService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -36,6 +38,10 @@ public class SubscriptionTypeServiceImpl implements SubscriptionTypeService {
 
     @Override
     public SubscriptionType create(SubscriptionTypeDto dto) {
+        if (Objects.nonNull(dto.getId())) {
+            throw new BadRequestException("Id deve ser nulo");
+        }
+
         return subscriptionTypeRepository.save(SubscriptionType.builder()
                         .id(dto.getId())
                         .name(dto.getName())
